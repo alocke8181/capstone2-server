1d7cc6697f62994e44cdd5a2dd22415a62f4c834
alocke8181 <alocke_8181@yahoo.com>
2024-02-17 14:59:25 -0500

    fix for deploy

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 const PORT = +process.env.PORT || 3001;
 
 function getDatabaseUri(){
-    return(process.env.NODE_ENV === "test") 
+    return(process.env.NODE_ENV === "test" 
     ? 'capstone2_test'
-    : process.env.DATABASE_URL || 'capstone2'
+    : process.env.DATABASE_URL || 'capstone2')
 }
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
